2cd77a7cc7938f2635041fe328c7d47b145f1f30
Lilisys <jesswu@umich.edu>
2016-07-01 13:12:43 -0400

    adding images from json

--- statescript.js
+++ statescript.js
@@ -28,6 +28,56 @@ app.controller('landingCtrl', function($scope){
 	document.body.className = "landing";
 });
 
-app.controller('galleryCtrl', function($scope){
+app.controller('galleryCtrl', function($scope, $http){
 	document.body.className = "";
+	var pics = [];
+
+	var allPics = {}
+
+	//Grab them from a local json
+	$http.get('images.json')
+	   .then(function(result){
+	      $scope.artwork = result.data;       
+	      console.log($scope.artwork);         
+	    });
+
+//	$scope.artwork = pics;
+
+	//Modal Stuff
+	var modal = document.getElementById('myModal');
+	var span = document.getElementsByClassName("close")[0];
+	console.log(span)
+
+
+	$scope.modalOpen = function(source){
+		modal.style.display='block';
+		var newImg = document.createElement("img");
+		//document.getElementById('modal-text').innerHTML = text;
+		document.getElementsByClassName('modal-img')[0].appendChild(newImg);
+		newImg.src = source;
+	};
+
+
+	//Delete the newly created image elt so 
+	function clearImg(){
+	  var delImg = document.getElementsByClassName('modal-img')[0]
+		console.log(delImg);
+		delImg.removeChild(delImg.firstChild);
+	}
+
+	//When you click the x, the modal window closes
+	$scope.clickX = function() {
+	  modal.style.display='none';
+	  clearImg();
+	}
+
+	//When you click outside the modal window, it closes
+	window.onclick = function(event){
+		if(event.target == modal){
+			modal.style.display='none'
+			console.log("clicked outside")
+			clearImg();
+		}
+	}
+
 });
