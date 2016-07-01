e463578512373119845284bad753a8b06e595c6d
Lilisys <jesswu@umich.edu>
2016-07-01 11:09:54 -0400

    working landing image

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # HFArtSite
 Making an Art Website for myself
+
+
+[Landing Page Background Image](https://css-tricks.com/perfect-full-page-background-image/)
--- statescript.js
+++ statescript.js
@@ -1,22 +1,33 @@
 var app = angular.module('artwork', ['ui.router']);
-app.config(function($stateProvider, $urlRouterProvider){
+
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider){
 	
-	$urlRouterProvider.otherwise("/state1");
+	$urlRouterProvider.otherwise("/landing");
 
 	$stateProvider
-		.state('state1', {
-			url: "/state1",
-			templateUrl: 'partials/landing.html'
+		.state('landing', {
+			url: "/landing",
+			templateUrl: 'partials/landing.html',
+			controller: 'landingCtrl'
 		})
-		.state('state1.list', {
-			url: "/list",
-			templateUrl: "partials/state1.list.html",
-			controller: function($scope){
-				$scope.items = ["A", "list", "of", "items"];
-			}
-		})
-		.state('state2', {
-			url: "/state2",
-			templateUrl: 'partials/gallery.html'
+		// .state('state1.list', {
+		// 	url: "/list",
+		// 	templateUrl: "partials/state1.list.html",
+		// 	controller: function($scope){
+		// 		$scope.items = ["A", "list", "of", "items"];
+		// 	}
+		// })
+		.state('gallery', {
+			url: "/gallery",
+			templateUrl: 'partials/gallery.html',
+			controller: 'galleryCtrl'
 		});
+});
+
+app.controller('landingCtrl', function($scope){
+	document.body.className = "landing";
+});
+
+app.controller('galleryCtrl', function($scope){
+	document.body.className = "";
 });
